eff5f3b493e2aafd12c625d7460ad8d40050b8e5
cihanbahcivan <cihanbhcvn@gmail.com>
2024-07-18 17:23:26 +0300

    such

---
+++ Entities/Concrete/Address.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities;
+
+namespace Entities.Concrete
+{
+    public class Address : IEntity
+    {
+        public int Id { get; set; }
+        public int CityId { get; set; }
+        public int DistrictId { get; set; }
+        public int NeighbourhoodId { get; set; }
+        public int StreetId { get; set; }
+        public string? ApartmentName { get; set; } = null;
+        public string? Floor { get; set; }
+        public string? DoorNumber { get; set; }
+
+
+    }
+}
--- Entities/Concrete/PersonalInformation.cs
+++ Entities/Concrete/PersonalInformation.cs
@@ -6,14 +6,14 @@ namespace Entities.Concrete
     {
         public int Id { get; set; }
         public int MemberId { get; set; }
+        public int AddressId { get; set; }
         public string TCNumber { get; set; }
         public string Gender { get; set; }
-        public string MembershipStatus { get; set; }
         public string MarriageStatus { get; set; }
         public string BloodType { get; set; }
         public string DrivingLicense { get; set; }
+        public virtual Member Member { get; set; }
+        public virtual Address Address { get; set; }
 
     }
-
-
 }
