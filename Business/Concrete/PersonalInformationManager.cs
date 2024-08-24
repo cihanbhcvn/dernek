0285532f248f3b347af45df1d33399fd87e0377f
cihanbahcivan <cihanbhcvn@gmail.com>
2024-08-24 23:18:30 +0300

    Example manager yet, awating update due to further development

--- Business/Concrete/PersonalInformationManager.cs
+++ Business/Concrete/PersonalInformationManager.cs
@@ -1,41 +1,59 @@
 ﻿using Business.Abstract;
+using Business.CCS;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
+using Core.Utilities.Business;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Business.Concrete
 {
     public class PersonalInformationManager : IPersonalInformationService
     {
-        private IPersonalInformationDal _personalInformationDal;
+        #region CTOR
+
+
+        private readonly IPersonalInformationDal _personalInformationDal;
+        private readonly ILogger _logger;
+
 
         public PersonalInformationManager(IPersonalInformationDal personalInformationDal)
         {
             _personalInformationDal = personalInformationDal;
         }
+
+
+        #endregion CTOR
+
+        #region Endpoints 
+
+
+        [ValidationAspect(typeof(PersonalInformationValidator))]
         public IResult Add(PersonalInformation personalInformation)
         {
-            try
+            var result = BusinessRules.Run();
+            if(true /*result*/) // İş kuralları
             {
                 _personalInformationDal.Add(personalInformation);
+                return new SuccessResult();
             }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
+            
 
+            //return new ErrorResult(result.Message);
+        }
+
+        public IResult Update(PersonalInformation personalInformation)
+        {
+            _personalInformationDal.Update(personalInformation);
             return new SuccessResult();
-
         }
 
         public IResult Delete(PersonalInformation personalInformation)
         {
             _personalInformationDal.Delete(personalInformation);
+
             return new SuccessResult();
         }
 
@@ -49,10 +67,17 @@ namespace Business.Concrete
             return new SuccessDataResult<PersonalInformation>(_personalInformationDal.Get(x => x.Id == id));
         }
 
-        public IResult Update(PersonalInformation personalInformation)
+
+        #endregion Endpoints
+
+        #region BusinessRules
+        // In order to run these rules in BusinessRules.Run() method, they must return IResult
+        
+        private IResult Example()
         {
-            _personalInformationDal.Update(personalInformation);
             return new SuccessResult();
         }
+
+        #endregion BusinessRules
     }
 }
